452aab01e168564c60f0452274034183dd1437d3
Alex <olivesargreek@gmail.com>
2022-07-14 11:09:41 +0300

    init: react-template webpack config

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './img/logo-og.png';
+
+const Module = () => (
+  <>
+    <h1>Hello React!!</h1>
+    <img src={logo} width={200} />
+  </>
+);
+
+export default Module;
